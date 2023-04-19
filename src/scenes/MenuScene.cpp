4e5c124ccfdb3c6dd3c0ef1fc97a5b151998d1e1
James51332 <jamesddadabo@gmail.com>
2023-04-18 21:48:24 -0500

    Use timestep for title pulses

--- src/scenes/MenuScene.cpp
+++ src/scenes/MenuScene.cpp
@@ -11,51 +11,29 @@
 namespace ltrm
 {
 
-MenuScene::MenuScene()
-{
-  Animation titleAnim;
-  titleAnim.Min = -300;
-  titleAnim.Max = 300;
-  titleAnim.Duration = 0.5;
-  titleAnim.Type = AnimationType::Wave;
-  titleAnim.ResetOnComplete = true;
-  titleAnim.Loop = false;
-  titleAnim.Active = false;
-  m_TitleAnimID = Animator::RegisterAnimation(titleAnim);
-}
-
 void MenuScene::Load()
 {
-  m_LastTime = SDL_GetTicks();
 }
 
 void MenuScene::Unload()
 {
-  
 }
 
 void MenuScene::Update(float timestep)
 {
   Renderer::Clear(Color::Accent);
   
-  float time = SDL_GetTicks();
-  m_PulseTime += time - m_LastTime;
-  m_LastTime = time;
-  if (m_PulseTime >= 3000.0f)
+  m_PulseTime += timestep;
+  if (m_PulseTime >= 3.0f)
   {
     m_PulseTime = 0.0f;
     UI::PulseTiles();
   }
-  
-  // TITLE
-  {
-    if (Input::KeyPress(SDLK_a)) Animator::SetAnimationActive(m_TitleAnimID);
-    
-    float x = Renderer::GetWidth() / 2 + Animator::QueryAnimation(m_TitleAnimID).Value;
-    float y = Style::SmallMargin + Style::TileSize / 2;
-    UI::TiledText(std::string("Letter"), x, y, 2);
-    UI::TiledText(std::string("morph"), x, y + Style::TileSize + Style::SmallMargin, 2);
-  }
+
+  float x = Renderer::GetWidth() / 2;
+  float y = Style::SmallMargin + Style::TileSize / 2;
+  UI::TiledText(std::string("Letter"), x, y, 2);
+  UI::TiledText(std::string("morph"), x, y + Style::TileSize + Style::SmallMargin, 2);
   
 	constexpr float btnWidth = 800;
   constexpr float btnHeight = 120;
--- src/scenes/MenuScene.h
+++ src/scenes/MenuScene.h
@@ -10,16 +10,13 @@ namespace ltrm
 class MenuScene : public Scene
 {
 public:
-  MenuScene();
-  
   void Load();
   void Unload();
   void Update(float timestep);
   void KeyDown(SDL_Keycode key);
   
 private:
-  int m_TitleAnimID;
-  float m_PulseTime = 0, m_LastTime;
+  float m_PulseTime = 0;
 };
 
 }
